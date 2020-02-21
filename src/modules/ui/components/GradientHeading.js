60e7259511a09043d69ea82361503c64cea0a7bc
Rizchel Wood <rizchelwood@gmail.com>
2020-02-21 14:05:53 -0700

    Add gradient heading component

---
+++ src/modules/ui/components/GradientHeading.js
@@ -0,0 +1,45 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+// Tempoarary until theme-ui is set up, these can be added to theme-ui instead
+const sizeStyles = {
+  S: {
+    fontSize: `1.5rem`,
+    lineHeight: `1.75rem`,
+  },
+  M: {
+    fontSize: `3rem`,
+    lineHeight: `3rem`,
+  },
+  L: {
+    fontSize: `3.5rem`,
+    lineHeight: `4rem`,
+  },
+}
+
+const propTypes = {
+  children: PropTypes.string,
+  leftColor: PropTypes.string,
+  rightColor: PropTypes.string,
+  size: PropTypes.string,
+}
+
+const GRADIENT_ANGLE = `45deg`
+
+const GradientHeading = ({ children, leftColor, rightColor, className, size = "M" }) => (
+  <h1
+    className={className}
+    css={{
+      ...sizeStyles[size],
+      background: `linear-gradient(${GRADIENT_ANGLE}, ${leftColor}, ${rightColor})`,
+      "-webkit-background-clip": `text`,
+      "-webkit-text-fill-color": `transparent`,
+    }}
+  >
+    {children}
+  </h1>
+)
+
+GradientHeading.propTypes = propTypes
+
+export default GradientHeading
