a0286fb29abb05e3760424adfe5aa6545950c650
greg lobinski <greglobinski@gmail.com>
2020-02-24 12:46:39 +0100

    feat: theme-ui + layout + global styles

---
+++ src/pages/chart-demo.js
@@ -0,0 +1,8 @@
+import React from "react"
+import Layout from "@modules/ui/components/Layout"
+
+const ChartDemoPage = () => {
+  return <Layout>Chart....</Layout>
+}
+
+export default ChartDemoPage
