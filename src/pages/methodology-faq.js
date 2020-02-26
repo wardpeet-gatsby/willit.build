73c7df1b28e7f70b1cad7b9beda12a080c55b6a1
Josh Comeau <joshwcomeau@gmail.com>
2020-02-26 13:31:45 -0500

    Convert methodology

--- src/pages/methodology-faq.js
+++ src/pages/methodology-faq.js
@@ -3,10 +3,6 @@ import React from "react"
 import Layout from "@modules/ui/components/Layout"
 import Docs from "@modules/docs/components/Docs"
 
-const Default = () => (
-  <Layout>
-    <Docs />
-  </Layout>
-)
+const Default = () => <h1>Willit.build Methodology + FAQ page</h1>
 
 export default Default
