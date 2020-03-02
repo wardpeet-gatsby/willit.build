d561cbef725934a02a9346a018a0830c7c177130
Josh Comeau <joshwcomeau@gmail.com>
2020-03-02 16:44:08 -0500

    WIP

--- src/modules/ui/components/SelectControl.js
+++ src/modules/ui/components/SelectControl.js
@@ -1,7 +1,24 @@
 import React from "react"
 
-const SelectControl = ({ children }) => {
-  return <div>{children}</div>
+import useRandomId from "../hooks/use-random-id.hook"
+
+const SelectControl = ({ label, initialValue, children }) => {
+  const randomId = useRandomId("select-control")
+
+  return (
+    <div>
+      <label htmlFor={`${randomId}`}>{label}</label>
+      <select value={initialValue}>{children}</select>
+    </div>
+  )
+}
+
+export const SelectControlOption = ({ id, url, children }) => {
+  return (
+    <option value={id} data-url={url}>
+      {children}
+    </option>
+  )
 }
 
 export default SelectControl
