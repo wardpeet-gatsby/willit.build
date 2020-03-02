d9b7606d89a552253c20143d5677b30eca6537e0
Josh Comeau <joshwcomeau@gmail.com>
2020-03-02 16:51:59 -0500

    Remove console.log

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,6 @@ module.exports = {
   stories: ["../src/modules/**/*.stories.js"],
   addons: ["@storybook/addon-actions", "@storybook/addon-links"],
   webpackFinal: async config => {
-    console.log(config)
     config.resolve.alias = moduleAliases
 
     return config
