dc42d2ecb66b198a541291dcb3f27c50d8546183
Joshua Comeau <joshwcomeau@gmail.com>
2020-03-24 14:29:10 -0400

    Redirect to API playground (#59)
    * Redirect to API playground
    
    * Switch to netlify.toml config
    
    * Remove unnecessary test

--- audit/cypress/integration/pages/all-pages.js
+++ audit/cypress/integration/pages/all-pages.js
@@ -21,17 +21,6 @@ context("All pages", () => {
     })
   })
 
-  it("audits the api-playground", () => {
-    cy.visit("/api-playground")
-    cy.audit({
-      performance: 90,
-      accessibility: 100,
-      "best-practices": 78,
-      seo: 70,
-      pwa: 59,
-    })
-  })
-
   it("audits the calculator", () => {
     cy.visit("/calculator/type/blog/source/drupal/page-count/512")
     cy.audit({
--- src/pages/api-playground.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const ApiPlayground = () => {
-  return <h1>API Playground</h1>
-}
-
-export default ApiPlayground
